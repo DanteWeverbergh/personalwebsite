c95bc2f157efed99d911d3fb4c591eda0b69e13a
{DanteWeverbergh} <{dantweve@student.arteveldehs.be}>
2022-12-07 16:53:12 +0100

    projects

--- src/Components/Cards/Projects/ProjectCard.js
+++ src/Components/Cards/Projects/ProjectCard.js
@@ -1,25 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { FaLinkedin, FaGithubSquare } from 'react-icons/fa';
 
 function ProjectCard({ project }) {
+  const ref = useRef(null);
+
   useEffect(() => {
     //
-    console.log(project.title);
+    console.log(project.tags);
   }, []);
 
   return (
     <>
       <div className="relative md:w-96 bg-slate-950 p-6 md:p-0 md:bg-blue-950 rounded-lg ">
-        <img
-          src={project.img ? project.img : 'https://picsum.photos/200/300'}
-          className="w-96 h-56 object-cover rounded-lg"
-        />
+        <div className="relative cursor-pointer" ref={ref}>
+          <img
+            src={project.img ? project.img : 'https://picsum.photos/200/300'}
+            className="w-96 h-56 object-cover rounded-lg"
+            id="image"
+          />
+          <h1
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-md bg-slate-950 px-4 py-2 rounded-lg "
+            id="image_click"
+          >
+            Click to visit website.
+          </h1>
+        </div>
+
         <div>
           <div>
-            <h1 className="font-bold text-2xl my-4">{project.title}</h1>
-            <p className="mb-12">{project.description}</p>
-            <div className="bg-blue-950  md:bg-slate-950 bottom-0 px-4 py-2 rounded-lg absolute mb-6 md:mb-0 hover:bg-white-950 hover:text-slate-950">
-              Read more
+            <h1 className="font-bold text-2xl mt-4 mb-4">{project.title}</h1>
+
+            <p className="mb-2">{project.description}</p>
+            <div className="flex gap-2 ">
+              {project.tags.map((tag) => (
+                <h3 className="bg-slate-950 rounded-lg px-2 py-1">{tag}</h3>
+              ))}
             </div>
           </div>
         </div>
--- src/Pages/Home/Components/Data.js
+++ src/Pages/Home/Components/Data.js
@@ -4,17 +4,20 @@ export const projects = [
     description:
       '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
     img: '/img/projects/gains_mockup_transparant.png',
+    tags: ['React', 'mobile'],
   },
   {
     title: 'Custom cms website',
     description:
       '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
     img: '/img/projects/gains_mockup_transparant.png',
+    tags: ['Laravel', 'responsive'],
   },
   {
     title: 'Photograpy portfolio',
     description:
       '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
-    img: '/img/projects/gains_mockup.png',
+    img: '/img/projects/photography_portfolio.png',
+    tags: ['React', 'responsive'],
   },
 ];
